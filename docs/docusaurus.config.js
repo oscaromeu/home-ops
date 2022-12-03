e20f11c357096d1897b17178647f4a1a36e50d36
Oscar Romeu <oscaromeu@gmail.com>
2022-12-03 17:16:54 +0100

    Configure docs and blogs edit urls

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -38,14 +38,14 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/${organizationName}/${projectName}/tree/main/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/${organizationName}/${projectName}/tree/main/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
