01e3b267e8af70be082775ec0b4ad1d6633728a8
Oscar Romeu <oscaromeu@gmail.com>
2022-12-03 17:05:04 +0100

    Generate docs with yarn

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -4,20 +4,21 @@
 const lightCodeTheme = require('prism-react-renderer/themes/github');
 const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
+const organizationName = "oscaromeu";
+const projectName = "live-infra";
+
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Live-Infra',
+  title: 'My Site',
   tagline: 'Dinosaurs are cool',
-  url: 'https://oscaromeu.github.io/live-infra',
-  baseUrl: '/',
+  url: 'https://${organizationName}.github.io',
+  baseUrl: '/${projectName}/',
+  organizationName, // GitHub org/user name.
+  projectName, // Usually your repo name.
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
 
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'oscaromeu', // Usually your GitHub org/user name.
-  projectName: 'live-infra', // Usually your repo name.
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
