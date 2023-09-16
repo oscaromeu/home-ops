cde0cd613ba02f2f94c9f88a133a383ecbce34ee
Oscar Romeu <oscaromeu@gmail.com>
2023-09-16 17:33:29 +0200

    feat: add scheduled springboot load testing

---
+++ kubernetes/apps/dev/spring-boot-app/monitoring/tests/test/app-a-2.js
@@ -0,0 +1,24 @@
+import http from "k6/http";
+import { check, sleep } from "k6";
+
+// Test configuration
+export const options = {
+  thresholds: {
+    // Assert that 99% of requests finish within 3000ms.
+    http_req_duration: ["p(99) < 3000"],
+  },
+  // Ramp the number of virtual users up and down
+  stages: [
+    { duration: "30s", target: 15 },
+    { duration: "1m", target: 15 },
+    { duration: "20s", target: 0 },
+  ],
+};
+
+// Simulated user behavior
+export default function () {
+  let res = http.get("http://app-a.dev.svc.cluster.local:8080/actuator/metrics");
+  // Validate response status
+  check(res, { "status was 200": (r) => r.status == 200 });
+  sleep(1);
+}
