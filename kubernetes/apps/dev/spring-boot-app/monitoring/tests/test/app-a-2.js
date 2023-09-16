58f02cc242fdad0da620f2adb460716fb6f6d3b2
Oscar Romeu <oscaromeu@gmail.com>
2023-09-16 20:27:31 +0200

    feat: Kame Hame Ha

--- kubernetes/apps/dev/spring-boot-app/monitoring/tests/test/app-a-2.js
+++ kubernetes/apps/dev/spring-boot-app/monitoring/tests/test/app-a-2.js
@@ -1,24 +1,37 @@
-import http from "k6/http";
-import { check, sleep } from "k6";
+// ...
 
-// Test configuration
-export const options = {
-  thresholds: {
-    // Assert that 99% of requests finish within 3000ms.
-    http_req_duration: ["p(99) < 3000"],
-  },
-  // Ramp the number of virtual users up and down
-  stages: [
-    { duration: "30s", target: 15 },
-    { duration: "1m", target: 15 },
-    { duration: "20s", target: 0 },
-  ],
-};
-
-// Simulated user behavior
 export default function () {
-  let res = http.get("http://app-a.dev.svc.cluster.local:8080/actuator/metrics");
-  // Validate response status
-  check(res, { "status was 200": (r) => r.status == 200 });
+  // First HTTP request
+  let res1 = http.get("http://app-a.dev.svc.cluster.local:8080");
+  check(res1, { "status was 200": (r) => r.status == 200 });
+
+  // Second HTTP request
+  let res2 = http.get("http://app-a.dev.svc.cluster.local:8080/io_task");
+  check(res2, { "status was 200": (r) => r.status == 200 });
+
+  // Second HTTP request
+  let res3 = http.get("http://app-a.dev.svc.cluster.local:8080/cpu_task");
+  check(res3, { "status was 200": (r) => r.status == 200 });
+
+  // Second HTTP request
+  let res4 = http.get("http://app-a.dev.svc.cluster.local:8080/random_sleep");
+  check(res4, { "status was 200": (r) => r.status == 200 });
+
+  // Second HTTP request
+  let res5 = http.get("http://app-a.dev.svc.cluster.local:8080/random_status");
+  check(res5, { "status was 200": (r) => r.status == 200 });
+
+  // Second HTTP request
+  let res6 = http.get("http://app-a.dev.svc.cluster.local:8080/chain");
+  check(res6, { "status was 200": (r) => r.status == 200 });
+
+  // Second HTTP request
+  let res7 = http.get("http://app-a.dev.svc.cluster.local:8080/error_test");
+  check(res7, { "status was 200": (r) => r.status == 200 });
+
+  // Second HTTP request
+  let res8 = http.get("http://app-a.dev.svc.cluster.local:8080/actuator/metrics");
+  check(res8, { "status was 200": (r) => r.status == 200 });
+
   sleep(1);
 }
