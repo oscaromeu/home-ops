207b495b7b67c1b2014726e9253acbb4fd4eae9e
Oscar Romeu <oscaromeu@gmail.com>
2023-09-18 07:27:53 +0200

    feat: add node exporter full dashboard

--- kubernetes/apps/dev/spring-boot-app/monitoring/tests/test/app-a-1.js
+++ kubernetes/apps/dev/spring-boot-app/monitoring/tests/test/app-a-1.js
@@ -15,10 +15,33 @@ export const options = {
   ],
 };
 
-// Simulated user behavior
 export default function () {
-  let res = http.get("http://app-a.dev.svc.cluster.local:8080");
-  // Validate response status
-  check(res, { "status was 200": (r) => r.status == 200 });
+
+  let res1 = http.get("http://app-a.dev.svc.cluster.local:8080");
+  check(res1, { "status was 200": (r) => r.status == 200 });
+
+
+  let res2 = http.get("http://app-a.dev.svc.cluster.local:8080/io_task");
+  check(res2, { "status was 200": (r) => r.status == 200 });
+
+
+  let res3 = http.get("http://app-a.dev.svc.cluster.local:8080/cpu_task");
+  check(res3, { "status was 200": (r) => r.status == 200 });
+
+  let res4 = http.get("http://app-a.dev.svc.cluster.local:8080/random_sleep");
+  check(res4, { "status was 200": (r) => r.status == 200 });
+
+  let res5 = http.get("http://app-a.dev.svc.cluster.local:8080/random_status");
+  check(res5, { "status was 200": (r) => r.status == 200 });
+
+  let res6 = http.get("http://app-a.dev.svc.cluster.local:8080/chain");
+  check(res6, { "status was 200": (r) => r.status == 200 });
+
+  let res7 = http.get("http://app-a.dev.svc.cluster.local:8080/error_test");
+  check(res7, { "status was 200": (r) => r.status == 200 });
+
+  let res8 = http.get("http://app-a.dev.svc.cluster.local:8080/actuator/prometheus");
+  check(res8, { "status was 200": (r) => r.status == 200 });
+
   sleep(1);
 }
