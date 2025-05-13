25672f5ce9cdf75d0fa22c55e104aa560b0a85bb
Oscar Romeu <oscaromeu@gmail.com>
2025-05-13 11:20:27 +0200

    feat: test browser

--- kubernetes/apps/observability/k6s/app/test.js
+++ kubernetes/apps/observability/k6s/app/test.js
@@ -1,16 +1,28 @@
-import http from 'k6/http';
-import { sleep } from 'k6';
+import { browser } from 'k6/browser';
 
 export const options = {
-  vus: 10,
-  iterations: 10,
+  scenarios: {
+    ui: {
+      executor: 'shared-iterations',
+      options: {
+        browser: {
+          type: 'chromium',
+        },
+      },
+    },
+  },
+  thresholds: {
+    checks: ['rate==1.0'],
+  },
 };
 
-// The default exported function is gonna be picked up by k6 as the entry point for the test script. It will be executed repeatedly in "iterations" for the whole duration of the test.
-export default function () {
-  // Make a GET request to the target URL
-  http.get('https://quickpizza.grafana.com');
+export default async function () {
+  const page = await browser.newPage();
 
-  // Sleep for 1 second to simulate real-world usage
-  sleep(1);
+  try {
+    await page.goto('https://test.k6.io/');
+    await page.screenshot({ path: 'screenshots/screenshot.png' });
+  } finally {
+    await page.close();
+  }
 }
