eee0f1e942f2a015dd452c3fcb932e02065cd4fc
Oscar Romeu <oscaromeu@gmail.com>
2025-06-10 17:23:36 +0200

    fix: add test

--- scripts/kubeconform.sh
+++ scripts/kubeconform.sh
@@ -28,15 +28,3 @@ find "${KUBERNETES_DIR}/clusters/" -maxdepth 1 -type f -name '*.yaml' -print0 | 
       exit 1
     fi
 done
-
-
-echo "=== Validating kustomizations in ${KUBERNETES_DIR}/infrastructure/ ==="
-find "${KUBERNETES_DIR}/infrastructure/" -type f -name $kustomize_config -print0 | while IFS= read -r -d $'\0' file;
-  do
-    echo "=== Validating kustomizations in ${file/%$kustomize_config} ==="
-    kustomize build "${file/%$kustomize_config}" "${kustomize_args[@]}" | \
-      kubeconform "${kubeconform_args[@]}"
-    if [[ ${PIPESTATUS[0]} != 0 ]]; then
-      exit 1
-    fi
-done
