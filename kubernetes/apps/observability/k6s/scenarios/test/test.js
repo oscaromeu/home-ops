7da7ff5444608fb31018667444f5649a6bcc4f03
Oscar Romeu <oscaromeu@gmail.com>
2026-01-08 12:16:27 +0100

    feat: add k6s

---
+++ kubernetes/apps/observability/k6s/scenarios/test/test.js
@@ -0,0 +1,9 @@
+import { check } from 'k6';
+import http from 'k6/http';
+
+export default function () {
+  const res = http.get('http://kube-prometheus-stack-prometheus.observability.svc.cluster.local:9090/metrics');
+  check(res, {
+    'is status 200': (r) => r.status === 200,
+  });
+}
