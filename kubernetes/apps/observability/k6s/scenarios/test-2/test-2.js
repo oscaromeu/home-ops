39a58df4293bb90b352641e04369f79fdf5ef31e
Oscar Romeu <oscaromeu@gmail.com>
2026-01-11 19:31:33 +0100

    feat: remove test-2

--- kubernetes/apps/observability/k6s/scenarios/test-2/test-2.js
+++
@@ -1,9 +0,0 @@
-import { check } from 'k6';
-import http from 'k6/http';
-
-export default function () {
-  const res = http.get('http://kube-prometheus-stack-prometheus.observability.svc.custer.local:9090/metrics');
-  check(res, {
-    'is status 200': (r) => r.status === 200,
-  });
-}
