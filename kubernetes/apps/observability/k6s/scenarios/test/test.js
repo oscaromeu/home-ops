bba5714974005b68e31be2f82752ef4e575d88ce
Oscar Romeu <oscaromeu@gmail.com>
2026-01-16 08:21:34 +0100

    feat: add ip check

--- kubernetes/apps/observability/k6s/scenarios/test/test.js
+++ kubernetes/apps/observability/k6s/scenarios/test/test.js
@@ -1,9 +1,21 @@
 import { check } from 'k6';
 import http from 'k6/http';
+import { Gauge } from 'k6/metrics';
+
+const ip_check = new Gauge('ip_check');
 
 export default function () {
-  const res = http.get('http://kube-prometheus-stack-prometheus.observability.svc.cluster.local:9090/metrics');
+  const res = http.get('https://ifconfig.me/all.json');
+
   check(res, {
-    'is status 200': (r) => r.status === 200,
+    'status is 200': (r) => r.status === 200,
   });
+
+  const body = JSON.parse(res.body);
+  const ip = body.ip_addr;
+
+  // Add numeric value with IP as tag
+  ip_check.add(1, { ip_addr: ip });
+
+  console.log(`IP: ${ip}`);
 }
