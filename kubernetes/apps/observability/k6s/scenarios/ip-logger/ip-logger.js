b172655c0ccb997e55640021770aa79cb8b0cdb1
Oscar Romeu <oscaromeu@gmail.com>
2026-05-26 11:56:07 +0200

    feat: rename k6 probe test to ip-logger

--- kubernetes/apps/observability/k6s/scenarios/ip-logger/ip-logger.js
+++ kubernetes/apps/observability/k6s/scenarios/ip-logger/ip-logger.js
@@ -3,6 +3,7 @@ import http from 'k6/http';
 import { Gauge } from 'k6/metrics';
 
 const ip_check = new Gauge('ip_check');
+const expected_ip = __ENV.EXPECTED_IP;
 
 export default function () {
   const res = http.get(`https://${__ENV.MY_HOSTNAME}`);
@@ -14,8 +15,15 @@ export default function () {
   const body = JSON.parse(res.body);
   const ip = body.ip_addr;
 
-  // Add numeric value with IP as tag
   ip_check.add(1, { ip_addr: ip });
-
   console.log(`IP: ${ip}`);
+
+  if (expected_ip && ip !== expected_ip) {
+    console.log(JSON.stringify({
+      event: 'ip_change',
+      expected: expected_ip,
+      actual: ip,
+      message: `Home IP changed from ${expected_ip} to ${ip}`,
+    }));
+  }
 }
